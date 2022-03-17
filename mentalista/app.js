52780f5caa6ff51a6a4d0607a929a2e9d903f550
Hebert Rocha <70274409+Hebert324@users.noreply.github.com>
2022-03-16 22:00:33 -0300

    Atualização

--- mentalista/app.js
+++ mentalista/app.js
@@ -4,23 +4,23 @@
 // while = enquanto - estrutura de repetição ou loop
 // != diferente
 
-var numeroSecreto = parseInt(Math.random() * 10)
-var tentativas = 3
+// var numeroSecreto = parseInt(Math.random() * 10)
+// var tentativas = 3
 
-while(tentativas > 0) {
-  var chute = parseInt(prompt("Digite um número entre 0 e 10"))
+// while(tentativas > 0) {
+//   var chute = parseInt(prompt("Digite um número entre 0 e 10"))
 
-  if (numeroSecreto == chute) {
-    document.write("<h2>Você Acertou!</h2>")
-    break
-  } else if(chute > numeroSecreto) {
-    alert("O número secreto é menor")
-    tentativas = tentativas - 1
-  } else if(chute < numeroSecreto) {
-    alert("O número secreto é maior")
-    tentativas = tentativas - 1
-  }
-}
-if (chute != numeroSecreto){
-    document.write("<h2>" + "Você errou a resposta era " + numeroSecreto + "</h2>")
-}
+//   if (numeroSecreto == chute) {
+//     document.write("<h2>Você Acertou!</h2>")
+//     break
+//   } else if(chute > numeroSecreto) {
+//     alert("O número secreto é menor")
+//     tentativas = tentativas - 1
+//   } else if(chute < numeroSecreto) {
+//     alert("O número secreto é maior")
+//     tentativas = tentativas - 1
+//   }
+// }
+// if (chute != numeroSecreto){
+//     document.write("<h2>" + "Você errou a resposta era " + numeroSecreto + "</h2>")
+// }
